5035d2caeea4989aa7770b1ee08c2d892514fef6
Nguyen Thi Ut Vien <97380870+nguyenthiutvien@users.noreply.github.com>
2023-06-25 15:29:02 +0700

    finish

--- client/src/api/Api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export async function productsData() {
-  const products = await axios.get(
-    "https://645de9688d08100293f1eb54.mockapi.io/products"
-  );
-  
-  return products;
-}
